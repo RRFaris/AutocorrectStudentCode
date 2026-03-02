78a87ad1ba291a345dcc5cbbf7863185f4319762
Ryan-dev12 <ryan.faris007@gmail.com>
2026-03-02 07:59:32 -0800

    This is my commit for when I worked on this from 8:30-900pm last night. I am commiting this now because I have the sat in an hour and I am afraid of losing all my code by quitting the application

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Autocorrect
@@ -17,8 +18,14 @@ public class Autocorrect {
      * @param words The dictionary of acceptable words.
      * @param threshold The maximum number of edits a suggestion can have.
      */
-    public Autocorrect(String[] words, int threshold) {
 
+    String[] words;
+    int threshold;
+
+
+    public Autocorrect(String[] words, int threshold) {
+        this.words = words;
+        this.threshold = threshold;
     }
 
     /**
@@ -28,10 +35,45 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
+        words = loadDictionary("small.txt");
+        threshold = 3;
+
+        int[] distances = new int[words.length];
+        int index = 0;
+
+        for (String word : words) {
+            distances[index] = findEditDistance(word, typed);
+            index++;
+        }
+
+        Arrays.sort(distances);
+
+        String[] close = new String[threshold];
+        for (int i = 0; i < threshold; i++) {
+            close[i] = distances[i];
+        }
 
         return new String[0];
     }
 
+    public static int findEditDistance(String word, String typed) {
+        int wLength = word.length();
+        int tLength = typed.length();
+
+        if (tLength == 0)
+            return wLength;
+
+        else if (wLength == 0)
+            return tLength;
+
+        else if (word.charAt(wLength-1) == typed.charAt(tLength-1))
+            return findEditDistance(word.substring(0,wLength-1), typed.substring(0,tLength-1));
+
+        else {
+            int min = Math.min(findEditDistance(word.substring(0, wLength-1), typed), findEditDistance(word, typed.substring(0, tLength-1)));
+            return 1 + Math.min(min, findEditDistance(word.substring(0, wLength-1), typed.substring(0, tLength-1));
+        }
+    }
 
     /**
      * Loads a dictionary of words from the provided textfiles in the dictionaries directory.
